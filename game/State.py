30e8bfa0282865d4022f07d85c6e0dea5ea920d5
karol <karol.kowalski95@gmail.com>
2019-07-04 13:21:33 +0200

    Adding players to game state

--- game/State.py
+++ game/State.py
@@ -1,11 +1,13 @@
+from game.players import Player
 
 class GameState(object):
 
-    def __init__(self, player_A, player_B, curr_step):
-        self.player_A = player_A
-        self.player_B = player_B
+    def __init__(self, cfg):
+        self.player_A = Player('Pyjter', cfg)
+        self.player_B = Player('Mati', cfg)
         self.current_player = None  # player_A.name or player_B.name
         self.step_no = None  # current game step number
 
     def is_terminal_state(self):
-        pass
+        """Check if game is over (one player lost)"""
+        return self.player_A.health == 0 or self.player_B.health == 0
--- game/engine.py
+++ game/engine.py
@@ -1,10 +1,16 @@
+from game.State import GameState
+
 class GameEngine(object):
     """
     Main loop of game
     """
 
-    def __init__(self):
-        pass
+    def __init__(self, cfg):
+        self.gameState = GameState(cfg)
 
     def run(self):
-        pass
+        while not self.gameState.is_terminal_state():
+            pass
+        # Update game state (increment stepNumber, set current player)
+        # Choose player and get turn from player
+        # Apply tyrn on game state
