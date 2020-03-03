b8a0828b6057e69131fda4e3b49e3829f647fd3f
kk0walski <karol.kowalski95@gmail.com>
2020-03-03 19:12:50 +0100

    Fix tests

--- Player_test.py
+++ Player_test.py
@@ -4,32 +4,38 @@ from game import config as cfg
 from game.State import GameState
 from game.cards.availableCards import get_all_available_cards
 from game.players.Player import BasePlayer
+from game.cards.card import MinionCard
 
 
 class TestStringMethods(unittest.TestCase):
 
+    def setUp(self):
+        self.playerA = BasePlayer("FIRST")
+        self.playerB = BasePlayer("SECOND")
+        self.state = GameState(self.playerA, self.playerB, curr_step=1)
+
     def test_player_init(self):
-        player = BasePlayer("TEST")
         cards = get_all_available_cards()
-        assert set(player.deck) == set(cards)
-        assert player.name == "TEST"
-        cards[0].name = "TEST_CARD"
-        assert player.deck != cards
-        assert player.mana == cfg.INITIAL_MANA
-        assert player.health == cfg.INITIAL_HEALTH
+        assert all(x in cards for x in self.playerA.deck)
+        assert all(x in cards for x in self.playerB.deck)
+        assert self.playerA.name == "FIRST"
+        assert self.playerA.deck != cards
+        assert self.playerA.mana == cfg.INITIAL_MANA + 1
+        assert self.playerA.health == cfg.INITIAL_HEALTH
+        assert self.playerB.name == "SECOND"
+        assert self.playerB.deck != cards
+        assert self.playerB.mana == cfg.INITIAL_MANA
+        assert self.playerB.health == cfg.INITIAL_HEALTH
 
 
     def test_state(self):
-        player1 = BasePlayer("FIRST")
-        player2 = BasePlayer("SECOND")
-        state = GameState(player1, player2, curr_step=1)
-        assert player1, player2 == state.get_players()
-        possibleActions = state.get_possible_actions()
-        assert len(possibleActions['attack_player']) == len([minion for minion in player1.minions if minion.can_attack])
-        assert len(possibleActions['minion_puts']) == len([card for card in player1.hand if card.cost <= player1.mana])
+        assert self.playerA, self.playerB == self.state.get_players()
+        possibleActions = self.state.get_possible_actions()
+        assert len(possibleActions['attack_player']) == len([minion for minion in self.playerA.minions if minion.can_attack])
+        assert len(possibleActions['minion_puts']) == len([card for card in self.playerA.hand if card.cost <= self.playerA.mana])
         assert len(possibleActions['end_turn']) == 1
-        assert len(player1.hand) == 4
-        assert len(player2.hand) == 4
+        assert len(self.playerA.hand) == 4
+        assert len(self.playerB.hand) == 4
 
 
 if __name__ == '__main__':
